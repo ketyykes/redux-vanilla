27be71913508d10f3ab495f200399bf8208cfbb7
ketyykes <xi4869@gmail.com>
2022-06-09 14:19:53 +0800

    沒有actionCreator

--- all.js
+++ all.js
@@ -1,29 +1,25 @@
 import { createStore } from 'redux'
 const initState = {
-    money: 1000,
+    money: 1000
 }
-const depositActionCreator = (payload) => (
-    {
-        type: 'DEPOSIT',
-        payload
-    })
-const withdrawActionCreator = (payload) => (
-    {
-        type: 'WITHDRAW', payload
-    }
-)
 function ATMReducer(state = initState, action) {
     switch (action.type) {
         case 'DEPOSIT':
-            return { money: state.money + action.payload }
+            return { money: state.money + 100 }
         case 'WITHDRAW':
-            return { money: state.money - action.payload }
+            return { money: state.money - 100 }
         default:
             return state
     }
 }
 let store = createStore(ATMReducer);
 store.subscribe(() => console.log(store.getState()));
-store.dispatch(depositActionCreator(100));
-store.dispatch(withdrawActionCreator(200));
-store.dispatch(depositActionCreator(500));
+store.dispatch({
+    type: 'WITHDRAW'
+});
+store.dispatch({
+    type: 'DEPOSIT'
+});
+store.dispatch({
+    type: 'WITHDRAW'
+});
